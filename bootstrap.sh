343a9a1e0ed83791db07e979e02e9aa56dd7533d
Jamis Johnson <jamismanwaring@gmail.com>
2015-03-12 00:20:52 -0400

    adds atari 2600 roms download to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,20 @@ apt-get update
 apt-get install -y git vim
 
 cd /vagrant/Human_Level_Control_through_Deep_Reinforcement_Learning/
-
 if [ -d "torch" ]; then
+    echo "Dependencies already installed"
+else
+    # install lua, torch, other dqn dependencies
     ./install_dependencies.sh
+
+    # download atari 2600 rom library
+    wget http://www.atarimania.com/roms/Roms.zip
+    unzip Roms.zip -d roms
+    rm Roms.zip
+    cd roms/
+    rm HARMONY\ CART\ ROMS.zip
+    unzip ROMS.zip
+    rm ROMS.zip
+    mv ROMS/* .
+    rm -rf ROMS
 fi
