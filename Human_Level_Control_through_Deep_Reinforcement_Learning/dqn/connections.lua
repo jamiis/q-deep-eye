1448d9e5893a340a2cb057d8e61fbd493b3c2525
du48s03 <u5b890402@gmail.com>
2015-05-09 22:16:58 -0400

    Added the skeleton code for connections.lua

---
+++ Human_Level_Control_through_Deep_Reinforcement_Learning/dqn/connections.lua
@@ -0,0 +1,19 @@
+
+-- returns the connection to the slave servers
+-- inputs: servers   a list of server ips
+-- returns: connects a list of connections
+
+function initialize_connections( ... )
+	-- body
+end
+
+-- A connection looks something like this:
+-- local con = {ip="52.5.123.123", ......}
+
+
+-- inputs: 	state		The preprocessed screen
+--			connect 	A coonection object
+-- returns: action 		The action returned by the slave
+function get_action( ... )
+	-- body
+end
